31cceea260117b1f4ec39b266115e1c2028e6642
YSer <yoseri430@gmail.com>
2023-01-21 09:04:04 +0900

    first commit.

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>こんにちは</h1>
+    <p>正しく動かないと、エラーが返るようだ</p>
+    <?php for ($i = 0; $i < 10; $i++) : ?>
+        <p>da</p>
+    <?php endfor ?>
+</body>
+</html>
